dc8dfb27245152fb7cf75527a510082fb9b8e93b
Paul Hoerenz <development@hoerenz.com>
2025-04-08 22:39:45 +0200

    feat: page layout using shadcn

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,52 @@
 <script lang="ts">
+  import { page } from "$app/state";
+  import { Flag } from "lucide-svelte";
+
   import "../app.css";
 
-  let { children } = $props();
+  const { children } = $props();
+
+  const links = [
+    {
+      name: "Scoreboard",
+      href: "/scoreboard",
+    },
+    {
+      name: "Challenges",
+      href: "/challenges",
+    },
+    {
+      name: "Feed",
+      href: "/feed",
+    },
+  ];
 </script>
 
-{@render children()}
+<div class="flex min-h-screen w-full flex-col">
+  <header
+    class="bg-background sticky top-0 flex h-16 items-center gap-4 border-b px-4 md:px-6"
+  >
+    <nav class="flex flex-row items-center gap-5 text-sm font-medium lg:gap-6">
+      <a
+        href="/"
+        class="flex items-center gap-2 text-lg font-semibold md:text-base"
+      >
+        <Flag class="h-6 w-6" />
+        <span class="sr-only">zeroday.pw</span>
+      </a>
+
+      {#each links as { name, href }}
+        <a
+          {href}
+          class={`${page.url.pathname.startsWith(href) ? "text-foreground" : "text-muted-foreground"} hover:text-foreground transition-colors`}
+        >
+          {name}
+        </a>
+      {/each}
+    </nav>
+  </header>
+
+  <main class="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+    {@render children()}
+  </main>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,73 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-  documentation
-</p>
+<div>
+  <h1>zeroday.pw</h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex
+    sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis
+    convallis. Tempus leo eu aenean sed diam urna tempor.
+  </p>
+</div>
+
+<div>
+  <h2>Rules</h2>
+  <p>
+    Ut hendrerit semper vel class aptent taciti sociosqu. <a href="#"
+      >Quisque faucibus</a
+    > ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis
+    convallis. Tempus leo eu aenean sed diam urna tempor.
+  </p>
+
+  <ul>
+    <li>
+      Ad litora torquent per conubia nostra inceptos himenaeos. Lorem ipsum
+      dolor sit amet consectetur adipiscing elit.
+    </li>
+    <li>
+      Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus
+      ex sapien vitae pellentesque sem placerat.
+    </li>
+    <li>
+      In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed
+      diam urna tempor.
+    </li>
+    <li>
+      Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa
+      nisl malesuada lacinia integer nunc posuere.
+    </li>
+  </ul>
+</div>
+
+<style>
+  @import "$lib/tailwind.css";
+
+  h1 {
+    @apply scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl;
+  }
+
+  h2 {
+    @apply scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0;
+  }
+
+  h3 {
+    @apply scroll-m-20 text-2xl font-semibold tracking-tight;
+  }
+
+  h4 {
+    @apply scroll-m-20 text-2xl font-semibold tracking-tight;
+  }
+
+  p {
+    @apply leading-7 [&:not(:first-child)]:mt-6;
+  }
+
+  a {
+    @apply text-primary font-medium underline underline-offset-4;
+  }
+
+  blockquote {
+    @apply mt-6 border-l-2 pl-6 italic;
+  }
+
+  ul {
+    @apply my-6 ml-6 list-disc [&>li]:mt-2;
+  }
+</style>
