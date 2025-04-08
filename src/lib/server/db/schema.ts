c1fd7cc75ba1c07f7d1dc7214c3a50aa6f6edafe
Paul Hoerenz <development@hoerenz.com>
2025-04-08 21:09:59 +0200

    feat!: update database schema
    Commit generated Drizzle migrations and update all relevant fields to not null.

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -3,7 +3,7 @@ import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable("users", {
   id: integer().primaryKey(),
-  name: text().unique(),
+  name: text().unique().notNull(),
   // todo: authentication
 });
 export const usersRelations = relations(users, ({ many }) => ({
@@ -12,27 +12,29 @@ export const usersRelations = relations(users, ({ many }) => ({
 
 export const categories = sqliteTable("categories", {
   id: integer().primaryKey(),
-  name: text().unique(),
+  name: text().unique().notNull(),
 });
 
 export const challenges = sqliteTable("challenges", {
   id: integer().primaryKey(),
-  name: text().unique(),
-  flag: text(),
-  difficulty: text(),
-  categoryId: integer("category_id"),
+  name: text().unique().notNull(),
+  description: text().notNull(),
+  flag: text().notNull(),
+  dockerImage: text("docker_image").notNull(),
+  categoryId: integer("category_id").notNull(),
 });
-export const challengesRelations = relations(challenges, ({ one }) => ({
+export const challengesRelations = relations(challenges, ({ one, many }) => ({
   category: one(categories, {
     fields: [challenges.categoryId],
     references: [categories.id],
   }),
+  solves: many(solves),
 }));
 
 export const solves = sqliteTable("solves", {
   id: integer().primaryKey(),
-  userId: integer("user_id"),
-  challengeId: integer("challenge_id"),
+  userId: integer("user_id").notNull(),
+  challengeId: integer("challenge_id").notNull(),
   timestamp: text().default(sql`(CURRENT_TIMESTAMP)`),
 });
 export const solvesRelations = relations(solves, ({ one }) => ({
