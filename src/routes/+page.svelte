14e1ce50f788b1a8cbc744dc8b652d34588808f2
Paul Hoerenz <development@hoerenz.com>
2025-04-09 20:09:21 +0200

    refactor: move typography css to separate file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,37 +37,5 @@
 </div>
 
 <style>
-  @import "$lib/tailwind.css";
-
-  h1 {
-    @apply scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl;
-  }
-
-  h2 {
-    @apply scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0;
-  }
-
-  h3 {
-    @apply scroll-m-20 text-2xl font-semibold tracking-tight;
-  }
-
-  h4 {
-    @apply scroll-m-20 text-2xl font-semibold tracking-tight;
-  }
-
-  p {
-    @apply leading-7 [&:not(:first-child)]:mt-6;
-  }
-
-  a {
-    @apply text-primary font-medium underline underline-offset-4;
-  }
-
-  blockquote {
-    @apply mt-6 border-l-2 pl-6 italic;
-  }
-
-  ul {
-    @apply my-6 ml-6 list-disc [&>li]:mt-2;
-  }
+  @import "$lib/typography.css";
 </style>
