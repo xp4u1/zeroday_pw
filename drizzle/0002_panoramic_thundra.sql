7e7cd33629643f32d93b845d8b806bb823811eb9
Joel Greisl <205814388+Knutzer564@users.noreply.github.com>
2025-04-21 21:28:40 +0200

    fix: drizzle migration

---
+++ drizzle/0002_panoramic_thundra.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `sessions` (
+	`id` text PRIMARY KEY NOT NULL,
+	`user_id` text NOT NULL,
+	`expires_at` integer NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+PRAGMA foreign_keys=OFF;--> statement-breakpoint
+CREATE TABLE `__new_users` (
+	`id` text PRIMARY KEY NOT NULL,
+	`name` text NOT NULL,
+	`password_hash` text NOT NULL
+);
+--> statement-breakpoint
+INSERT INTO `__new_users`("id", "name", "password_hash") SELECT "id", "name", "password_hash" FROM `users`;--> statement-breakpoint
+DROP TABLE `users`;--> statement-breakpoint
+ALTER TABLE `__new_users` RENAME TO `users`;--> statement-breakpoint
+PRAGMA foreign_keys=ON;--> statement-breakpoint
+CREATE UNIQUE INDEX `users_name_unique` ON `users` (`name`);
