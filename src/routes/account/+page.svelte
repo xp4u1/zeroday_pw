f7da201bdf5969387535894c6dc7694122f76723
Paul Hoerenz <development@hoerenz.com>
2025-04-22 22:51:01 +0200

    feat(ui): account details
    Remove prototype and create real UI for the account details page

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,12 +1,44 @@
 <script lang="ts">
   import { enhance } from "$app/forms";
   import type { PageServerData } from "./$types";
+  import { Button } from "$lib/components/ui/button/index";
+  import * as Card from "$lib/components/ui/card/index";
 
   let { data }: { data: PageServerData } = $props();
 </script>
 
-<h1>Hi, {data.user.username}!</h1>
-<p>Your user ID is {data.user.id}.</p>
-<form method="post" action="?/logout" use:enhance>
-  <button>Sign out</button>
-</form>
+<Card.Root class="mx-auto max-w-3xl">
+  <Card.Header>
+    <Card.Title class="text-2xl">Hey {data.user.username}!</Card.Title>
+    <Card.Description
+      >Welcome to your profile page. Here you can see your progress and manage
+      your account</Card.Description
+    >
+  </Card.Header>
+  <Card.Content class="flex flex-col gap-5">
+    <p>
+      Solve challenges to collect flags and earn points. The more you solve, the
+      higher you climb on the leaderboard. You have completed <b
+        >{data.solves.length}</b
+      >
+      challenge{data.solves.length > 1 ? "s" : ""} so far{data.solves.length > 0
+        ? " — keep going!"
+        : "."}
+    </p>
+
+    {#if data.solves.length > 0}
+      <p>Completed challenges:</p>
+      <ul class="list-inside list-disc">
+        {#each data.solves as solve}
+          <li>{solve.challenge.name}</li>
+        {/each}
+      </ul>
+    {/if}
+
+    <p>Your user ID is <span class="font-mono">{data.user.id}</span>.</p>
+
+    <form method="post" action="?/logout" use:enhance>
+      <Button variant="outline" type="submit">Sign out</Button>
+    </form>
+  </Card.Content>
+</Card.Root>
