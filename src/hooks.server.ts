70c86de1540adc72a6e1cbcf41381e8f4806a9c4
Paul Hoerenz <development@hoerenz.com>
2025-04-23 21:00:29 +0200

    feat: garbage collector
    Remove unused Docker containers.
    
    Resolves #8

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,8 @@
 import type { Handle } from "@sveltejs/kit";
 import * as auth from "$lib/server/auth.js";
+import { setupCron } from "$lib/server/cron";
+
+setupCron();
 
 const handleAuth: Handle = async ({ event, resolve }) => {
   const sessionToken = event.cookies.get(auth.sessionCookieName);
