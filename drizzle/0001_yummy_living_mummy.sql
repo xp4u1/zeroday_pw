1eeda3f220083d9090e92c12c5f1b41ce9a84b1a
Paul Hoerenz <development@hoerenz.com>
2025-06-22 17:45:37 +0200

    chore(db)!: generate migrations
    BREAKING CHANGE: delete all incompatible (and broken) sqlite migrations

--- drizzle/0001_yummy_living_mummy.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `active_containers` (
-	`id` integer PRIMARY KEY NOT NULL,
-	`docker_id` text NOT NULL,
-	`address` text NOT NULL,
-	`user_id` integer NOT NULL,
-	`challenge_id` integer NOT NULL,
-	`timestamp` text DEFAULT (CURRENT_TIMESTAMP)
-);
--- drizzle/0004_breezy_lockheed.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `challenges` ADD `points` integer NOT NULL;
