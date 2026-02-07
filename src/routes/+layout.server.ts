775199cddd616aba74885f8349a1ffd3311db92b
Paul Hoerenz <development@hoerenz.com>
2026-02-08 00:36:57 +0100

    chore: format files using prettier

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,7 @@
-import type { LayoutServerLoad } from './$types';
+import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async (event) => {
-	return {
-		user: event.locals.user
-	};
+  return {
+    user: event.locals.user,
+  };
 };
